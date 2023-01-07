53df6a25ede4dcce96ff781d7aac7bfe62ed98f8
akashdas-tn-data-springml <akash.das@tn.gov>
2023-01-06 16:24:21 -0800

    adds feature to change directory to home

--- tf-test.sh
+++ tf-test.sh
@@ -3,10 +3,10 @@ set -e
 if [ $# -eq 1 ]; then
     COMMAND=$1
     module_dir=$(find ./modules -type d)
+    pwd=$(pwd)
     for dir in $module_dir
     do
         if [ "$dir" != "./modules" ]; then
-            echo $dir
             cd "$dir"
             if [ "$COMMAND" = "fmt" ]; then
                 for file in $(terraform fmt)
@@ -23,6 +23,7 @@ if [ $# -eq 1 ]; then
                 terraform init
                 terraform validate
             fi
+            cd "$pwd"
         fi
     done
 fi
