ef92db3203ff8ac8d0d26870e11ae1da35780364
akashdas-tn-data-springml <akash.das@tn.gov>
2023-01-06 16:26:49 -0800

    prints file that is not formatted

--- tf-test.sh
+++ tf-test.sh
@@ -12,6 +12,7 @@ if [ $# -eq 1 ]; then
                 for file in $(terraform fmt)
                 do 
                     if [ "${#file}" -gt 0 ]; then 
+                        echo "$file"
                         echo "******* Please format your tf files under $dir directory ********"
                         exit 1
                     else
