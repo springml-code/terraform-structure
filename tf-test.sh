b902b9bd78754dc3b439f9d3baa7067d5da5b7e0
akashdas-tn-data-springml <akash.das@tn.gov>
2023-01-06 16:20:19 -0800

    debugging tf actions

--- tf-test.sh
+++ tf-test.sh
@@ -5,22 +5,24 @@ if [ $# -eq 1 ]; then
     module_dir=$(find ./modules -type d)
     for dir in $module_dir
     do
-        cd "$dir"
-        if [ "$COMMAND" = "fmt" ]; then
-            for file in $(terraform fmt)
-            do 
-                if [ "${#file}" -gt 0 ]; then 
-                    echo "******* Please format your tf files under $dir directory ********"
-                    exit 1
-                else
-                    echo "Formatted Successfully"
-                fi
-            done
-        elif [ "$COMMAND" = "validate" ]; then
-            echo "$dir"
-            terraform init
-            terraform validate
+        if [ "$dir" != "./modules" ]; then
+            echo $dir
+            cd "$dir"
+            if [ "$COMMAND" = "fmt" ]; then
+                for file in $(terraform fmt)
+                do 
+                    if [ "${#file}" -gt 0 ]; then 
+                        echo "******* Please format your tf files under $dir directory ********"
+                        exit 1
+                    else
+                        echo "Formatted Successfully"
+                    fi
+                done
+            elif [ "$COMMAND" = "validate" ]; then
+                echo "$dir"
+                terraform init
+                terraform validate
+            fi
         fi
-        cd /workspace
     done
 fi
