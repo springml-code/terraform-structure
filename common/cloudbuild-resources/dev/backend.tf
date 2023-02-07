bf4a590df971889ad50a012deedecbf789645328
capt2101akash <akash.das@springml.com>
2023-02-06 16:36:18 -0800

    removes impersonation

--- common/cloudbuild-resources/dev/backend.tf
+++ common/cloudbuild-resources/dev/backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket                      = "cloud-build-tf-state-common"
     prefix                      = "resources/triggers"
-    impersonate_service_account = "sa-terraform-ci-cd@cloud-build-common-1884.iam.gserviceaccount.com"
   }
 }
